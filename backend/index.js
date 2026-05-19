9f4206ce631575ddadf51577a151d3330df25305
Nandini <nanduobi2006@gmail.com>
2026-05-19 06:55:11 +0530

    student result portal

---
+++ backend/index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+mongoose.connect(
+"mongodb://127.0.0.1:27017/academic_system"
+)
+
+.then(() => {
+
+    console.log("MongoDB Connected");
+
+});
+
+const studentSchema = new mongoose.Schema({
+
+    usn: String,
+    name: String,
+    semester: Number,
+
+    subjects: [
+
+        {
+            subjectCode: String,
+            subjectName: String,
+            marks: Number
+        }
+
+    ]
+
+});
+
+const Student = mongoose.model(
+    "students",
+    studentSchema
+);
+
+app.get("/student/:usn", async(req,res)=>{
+
+    const usn = req.params.usn;
+
+    const student =
+    await Student.findOne({ usn: usn });
+
+    if(student){
+
+        res.json(student);
+
+    }
+
+    else{
+
+        res.json({
+            message: "Student Not Found"
+        });
+
+    }
+
+});
+
+app.listen(3000, ()=>{
+
+    console.log(
+        "Server Running on Port 3000"
+    );
+
+});
---
+++ student result system/script.js
@@ -0,0 +1,38 @@
+async function getResult() {
+
+    const usn = document.getElementById("usn").value;
+
+    const response = await fetch(
+        `http://localhost:3000/student/${usn}`
+    );
+
+    const data = await response.json();
+
+    // show marks card
+    document.querySelector(".container").style.display 
+    = "block";
+
+    // student details
+    document.getElementById("student-usn").innerText = data.usn;
+    document.getElementById("student-name").innerText = data.name;
+    document.getElementById("semester").innerText = data.semester;
+
+    // table body
+    let tableRows = "";
+
+    data.subjects.forEach(subject => {
+
+        let result = subject.marks >= 35 ? "P" : "F";
+
+        tableRows += `
+        <tr>
+            <td>${subject.subjectCode}</td>
+            <td>${subject.subjectName}</td>
+            <td>${subject.marks}</td>
+            <td>${result}</td>
+        </tr>
+        `;
+    });
+
+    document.getElementById("marks-body").innerHTML = tableRows;
+}
