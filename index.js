8d93c9b2e6be2d6cd75983d5dc409544d0ccd6aa
Nandini <nanduobi2006@gmail.com>
2026-05-21 18:57:29 +0530

    fixed backend

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
-
+app.use(express.json());
 mongoose.connect(process.env.MONGO_URL)
 .then(() => {
     console.log("MongoDB Connected");
@@ -35,41 +35,34 @@ const studentSchema = new mongoose.Schema({
 });
 
 const Student = mongoose.model(
-    "students",
+    "student",
     studentSchema
 );
 
-app.get("/student/:usn", async(req, res) => {
+app.get("/student/:usn", async (req, res) => {
+    try {
+        const student = await Student.findOne({
+            usn: req.params.usn
+        });
 
-    const usn = req.params.usn;
-
-    const student =
-    await Student.findOne({ usn: usn });
-
-    if(student){
+        if (!student) {
+            return res.status(404).json({
+                message: "Student not found"
+            });
+        }
 
         res.json(student);
 
-    }
-
-    else{
-
-        res.json({
-
-            message: "Student Not Found"
-
+    } catch (error) {
+        res.status(500).json({
+            message: "Server Error"
         });
-
     }
-
 });
 
-app.listen(3000, () => {
+// server
+const PORT = process.env.PORT || 5000;
 
-    console.log(
-
-        "Server Running on Port 3000"
-
-    );
-
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
